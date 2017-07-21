eca9b0e787638b8030494844358b7ee4685b4840
Phil Burk <philburk@google.com>
2017-07-21 13:48:32 -0700

    msm8998-common: aaudio: increase burst size from 1 to 2 msec
    One msec is on the edge and sometimes glitches.
    Two msec is more stable and matches the FastMixer burst.
    
    Bug: 63934178
    Test: "adb shell getprop aaudio.hw_burst_min_usec" should be 2000
    Change-Id: I383c4a3a82283229ae39ca9fb6e2ffdd65ae0014

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -20,6 +20,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
+    aaudio.hw_burst_min_usec=2000 \
     aaudio.mmap_exclusive_policy=2 \
     aaudio.mmap_policy=2 \
     af.fast_track_multiplier=2 \
