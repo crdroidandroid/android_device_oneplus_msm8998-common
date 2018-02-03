300bdfb865a4eb5bcba402cc39d349ba3dc4dc7a
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    msm8998-common: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- common.mk
+++ common.mk
@@ -22,6 +22,8 @@
 # definition file).
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 
 # Setup dalvik vm configs
