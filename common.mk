8d4b9c1d5f0f7aeb736abddb4dc1098cdaf5fb6f
Pranav Vashi <neobuddy89@gmail.com>
2018-02-25 07:05:31 +0530

    msm8998: Add front cam hack for few apps
    * Like Whatsapp.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -22,8 +22,6 @@
 # definition file).
 #
 
-$(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
@@ -153,6 +151,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/vendor/lib/libmmcamera2_mct.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera2_mct.so \
+    $(LOCAL_PATH)/prebuilts/vendor/lib/libmmcamera_imx371.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_imx371.so
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
@@ -471,3 +473,4 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
+$(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
