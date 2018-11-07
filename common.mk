60875a0f3327fc8607f7fe1f2222f48f20e72c4e
Pranav Vashi <neobuddy89@gmail.com>
2018-11-07 20:01:55 +0530

    msm8998: Add missing libvulkan
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -176,7 +176,8 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.system \
+    libvulkan
 
 PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
