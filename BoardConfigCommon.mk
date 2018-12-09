138afa2e3715daad16ce2e6d7ef24b7c65be4fd1
Pranav Vashi <neobuddy89@gmail.com>
2018-12-09 11:48:06 +0530

    msm8998: Disable interaction boost
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -270,7 +270,7 @@ TARGET_USES_MKE2FS := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-TARGET_USES_INTERACTION_BOOST := true
+TARGET_USES_INTERACTION_BOOST := false
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
