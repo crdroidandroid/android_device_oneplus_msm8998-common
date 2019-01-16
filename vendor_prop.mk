cac312fdfbf9f0b6aa70844404ab1c903f9cf108
neobuddy89 <neobuddy89@gmail.com>
2019-01-17 01:57:55 +0530

    msm8998: Add for support wechat voice and video msg
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dirac.acs.controller=qem \
     ro.dirac.acs.storeSettings=1 \
     ro.dirac.ignore_error=1 \
-    tunnel.audio.encode=true
+    tunnel.audio.encode=true \
+    nxpWechatDebugEnable=1
 
 # Audio new
 PRODUCT_PROPERTY_OVERRIDES += \
