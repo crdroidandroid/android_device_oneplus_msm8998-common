8247b77e9ec301965754e1b9395b78b0a7be557c
neobuddy89 <neobuddy89@gmail.com>
2019-01-17 02:03:18 +0530

    msm8998: Disable connection tracking in DPM
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -110,7 +110,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.dpm.feature=11
+    persist.vendor.dpm.feature=9
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
