c30896c60f22137218b11743b37d4fc52213e025
Michael Bestas <mkbestas@lineageos.org>
2019-01-19 21:19:46 +0200

    msm8998-common: Retrieve time info from persist
    * This allows time to be correct on first boot
    
    Change-Id: I149722973a15e138b4ea111c4e05d186bb6883f5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -298,7 +298,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Timeservice
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.timed.enable=true
+    persist.vendor.delta_time.enable=true
 
 # Oiface
 PRODUCT_PROPERTY_OVERRIDES += \
