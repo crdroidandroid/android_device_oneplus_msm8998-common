2dc9fbd1e79fc78d5f8c665d094496fda59871ef
Pranav Vashi <neobuddy89@gmail.com>
2019-02-14 02:28:34 +0530

    DNM: msm8998: Switch to Snap2 temporarily
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -143,7 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap
+    Snap2
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
