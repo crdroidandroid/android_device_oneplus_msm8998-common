68a290daa508f4ffdbfaeb7541b1632ef018df35
Pranav Vashi <neobuddy89@gmail.com>
2019-03-31 11:55:53 +0530

    msm8998: Pre-opt SystemUI
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -155,6 +155,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
