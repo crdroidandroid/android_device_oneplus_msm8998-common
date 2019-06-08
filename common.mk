9b49e6f7d650b93375b6dc99f9661d59304aea00
Pranav Vashi <neobuddy89@gmail.com>
2019-06-09 02:21:47 +0530

    msm8998-common: Switch back to Snap cam app
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -142,7 +142,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap2
+    Snap
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
