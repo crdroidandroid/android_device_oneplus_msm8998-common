1dfcf4741b0b62a62381c3112d5c67d488b84d79
Sujin Panicker <spanic@codeaurora.org>
2019-07-16 18:52:05 +0530

    msm8998-common: Fix rank of QC OMX components
    Rank OMX SW codecs higher than C2 codecs except for opus codec.
    
    Change-Id: Id8d760e2fc8d33e8c8c97ed1ee10513c71457aa5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -196,6 +196,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
     media.stagefright.enable-player=true \
     media.stagefright.enable-http=true \
     media.stagefright.enable-aac=true \
