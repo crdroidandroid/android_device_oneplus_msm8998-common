490e3e2489fefae0592be771fb5154995ad94f71
Pranav Vashi <neobuddy89@gmail.com>
2019-10-06 11:32:22 +0530

    msm8998-common: Update to power HAL 1.2
    Change-Id: I386f42425e612eef7a14310bb4ca07a54efad272

--- common.mk
+++ common.mk
@@ -357,7 +357,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
