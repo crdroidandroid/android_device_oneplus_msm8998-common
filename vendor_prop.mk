bdddb4047adb0c140a481c53a6fcba06905b52f3
Pranav Vashi <neobuddy89@gmail.com>
2019-10-25 22:54:30 +0530

    Revert "msm8998-common: Force disable bluetooth audio hal 2.0"
    This reverts commit 91ef71e386f58f77341cdc3a1d02781b5868b53e.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -76,7 +76,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.bluetooth.bluetooth_audio_hal.disabled=true \
     bt.max.hfpclient.connections=1 \
     persist.bt.a2dp_offload_cap=false \
     vendor.qcom.bluetooth.soc=cherokee \
