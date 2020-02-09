1330b829fe6ed17927c9343fc99b3aa739402ba4
Pranav Vashi <neobuddy89@gmail.com>
2020-02-09 22:15:49 +0530

    msm8998-common: Use OnePlus camera
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -23,6 +23,7 @@
 #
 
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
+$(call inherit-product, vendor/addons/onepluscamera/config.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Overlays
