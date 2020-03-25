74f4a0419430e9903bdb424909987567e0f08874
Pranav Vashi <neobuddy89@gmail.com>
2020-03-26 00:41:25 +0530

    msm8998-common: Disable A2DP offload for now
    * This reverts f0b03bc135c3e2cb2be62d61b41b1e79bef137d5 partially.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -120,12 +120,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.max.hfpclient.connections=1 \
-    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
-    persist.bluetooth.a2dp_offload.disabled=false \
     persist.vendor.bluetooth.a4wp=false \
-    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
     persist.vendor.qcom.bluetooth.enable.splita2dp=true \
-    ro.bluetooth.a2dp_offload.supported=true \
     ro.vendor.bluetooth.wipower=false \
     vendor.bluetooth.emb_wp_mode=false \
     vendor.qcom.bluetooth.soc=cherokee
