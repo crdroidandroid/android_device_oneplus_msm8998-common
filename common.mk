296d6a2165d0031d4a4f2b1a816df74f50868952
Pranav Vashi <neobuddy89@gmail.com>
2020-04-14 03:32:48 +0530

    msm8998-common: Add missing QCOM and OnePlus WiFi HALs
    * Build what we can, copy rest.

--- common.mk
+++ common.mk
@@ -520,8 +520,14 @@ PRODUCT_PACKAGES += \
     hostapd \
     hostapd_cli \
     libqsap_sdk \
-    libQWiFiSoftApCfg \
+    libwpa_client \
+    libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    vendor.qti.hardware.fstman@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     wifi-mac-generator \
     wpa_supplicant \
     wpa_supplicant.conf
