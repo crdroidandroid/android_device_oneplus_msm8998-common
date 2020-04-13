2bfe77e36c007768572dc5f96a7f6a180bc81e78
Pranav Vashi <neobuddy89@gmail.com>
2020-04-14 03:32:48 +0530

    msm8998-common: Add vendor.qti.hardware.wifi
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -253,8 +253,7 @@ PRODUCT_PACKAGES += \
     libgeofencing \
     libgnss \
     libgnsspps \
-    libsynergy_loc_api \
-    libwifi-hal-ctrl
+    libsynergy_loc_api
 
 # GPS Configurations
 PRODUCT_PACKAGES += \
@@ -531,8 +530,13 @@ PRODUCT_PACKAGES += \
     hostapd \
     hostapd_cli \
     libqsap_sdk \
-    libQWiFiSoftApCfg \
+    libwpa_client \
+    libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf \
