dfa1d9cb54e605d97ae860288f622dcf49b279d9
Pranav Vashi <neobuddy89@gmail.com>
2020-04-16 22:11:12 +0530

    msm8998-common: Downgrade sound trigger to 2.1
    * Match the stock.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -93,7 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@5.0-util \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8998 \
