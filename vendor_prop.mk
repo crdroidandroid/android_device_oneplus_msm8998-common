252d1ce78a0a6039f2aa1c8d3fe09869c4b95460
Pranav Vashi <neobuddy89@gmail.com>
2020-04-21 23:55:37 +0530

    msm8998-common: Enable VoLTE on all by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -391,6 +391,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
 
+# VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
