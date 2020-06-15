6fb2fffd97e492755df1b952a21d5c6c69f05d3f
Nitin Shivpure <nshivpur@codeaurora.org>
2020-06-15 10:48:05 +0530

    msm8998-common: bt: Add vendor.qti.hardware.btconfigstore@2.0 into product package
    Add vendor.qti.hardware.btconfigstore@2.0 into product package.
    
    CRs-fixed: 2739162
    Change-Id: I36ca083bf333826ac8980541181aa52022671396

--- common.mk
+++ common.mk
@@ -166,7 +166,8 @@ PRODUCT_PACKAGES += \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_dun@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
