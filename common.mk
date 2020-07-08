5c062b467608be5624aeca188063300026e5d6d4
Pranav Vashi <neobuddy89@gmail.com>
2020-07-09 02:50:23 +0530

    msm8998-common: Use stock gps / location blobs
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -239,16 +239,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# GPS / Location
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti \
-    android.hardware.gnss@2.0-service-qti \
-    libbatching \
-    libgeofencing \
-    libgnss \
-    libgnsspps \
-    libsynergy_loc_api
-
 # GPS Configurations
 PRODUCT_PACKAGES += \
     apdr.conf \
