7625b8e976775018995653b9a9f5fabcce048671
Pranav Vashi <neobuddy89@gmail.com>
2020-09-02 01:49:44 +0530

    msm8998-common: Remove enable_gl_backpressure
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -368,8 +368,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.enable_gl_backpressure=1
+    debug.sf.early_gl_app_phase_offset_ns=15000000
 
 # Timeservice
 PRODUCT_PROPERTY_OVERRIDES += \
