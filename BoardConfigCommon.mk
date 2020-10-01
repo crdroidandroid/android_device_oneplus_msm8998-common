4c83c3610bf956194ec29750037ad75dca4edaed
Pranav Vashi <neobuddy89@gmail.com>
2020-10-01 16:36:54 +0530

    msm8998-common: sepolicy: Update for Android R
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iac51d61612fa22560440bdf9cf228e03806e1f12

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -237,7 +237,7 @@ BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
 VENDOR_SECURITY_PATCH := 2020-04-05
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
