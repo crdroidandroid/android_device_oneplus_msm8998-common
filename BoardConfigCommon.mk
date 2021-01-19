4cea46a7728a97b77adae51169f243d4111a6a6d
arco <arco68@gmail.com>
2021-01-20 02:16:47 +0700

    msm8998-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I1ebf0fb665704424a8ad3a375c546c6cca44dc0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,6 +40,7 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := oneplus5_defconfig
 
