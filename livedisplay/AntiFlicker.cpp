d7408b30550c0540564b1c97d2f40ddb8e6329b6
Pranav Vashi <neobuddy89@gmail.com>
2021-01-21 01:36:44 +0530

    msm8998-common: livedisplay: Add anti flicker support
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ livedisplay/AntiFlicker.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "AntiFlickerService"
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "AntiFlicker.h"
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace implementation {
+
+static constexpr const char* kProjectNamePath =
+        "/sys/project_info/project_name";
+static constexpr const char* kDcDimmingStatusPath =
+        "/proc/flicker_free/flicker_free";
+static constexpr const char* kDcDimmingBrightnessPath =
+        "/proc/flicker_free/min_brightness";
+
+Return<bool> AntiFlicker::isEnabled() {
+    std::string buf;
+    if (!android::base::ReadFileToString(kDcDimmingStatusPath, &buf)) {
+        LOG(ERROR) << "Failed to read " << kDcDimmingStatusPath;
+        return false;
+    }
+    return std::stoi(android::base::Trim(buf)) == 1;
+}
+
+Return<bool> AntiFlicker::showWarning() {
+    return false;
+}
+
+void set_min_brightness()
+{
+    std::string device;
+    if (android::base::ReadFileToString(kProjectNamePath, &device)) {
+        if (!strncmp(device.c_str(), "16859", 5)) {
+            // Oneplus 5
+            android::base::WriteStringToFile("66", kDcDimmingBrightnessPath);
+        }
+        else if (!strncmp(device.c_str(), "17801", 5)) {
+            // Oneplus 5T
+            android::base::WriteStringToFile("380", kDcDimmingBrightnessPath);
+        }
+    }
+}
+
+Return<bool> AntiFlicker::setEnabled(bool enabled) {
+    if (!android::base::WriteStringToFile((enabled ? "1" : "0"), kDcDimmingStatusPath)) {
+        LOG(ERROR) << "Failed to write " << kDcDimmingStatusPath;
+        return false;
+    }
+    set_min_brightness();
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
