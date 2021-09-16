696353c3b22e7d3171a03a5cf21486b4d16c0e53
Pranav Vashi <neobuddy89@gmail.com>
2021-09-17 03:39:01 +0530

    msm8998-common: Build sound trigger from source
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iac564fa668ce9c0ca254cd76f120b957d030bb16

--- common.mk
+++ common.mk
@@ -126,7 +126,8 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix \
     libtinycompress \
-    libtinycompress.vendor
+    libtinycompress.vendor \
+    sound_trigger.primary.msm8998:32
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
