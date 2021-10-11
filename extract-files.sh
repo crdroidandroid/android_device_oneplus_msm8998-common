4d5c6adf44b96a3cc09fe3912cc874b33c5876a0
Pranav Vashi <neobuddy89@gmail.com>
2021-10-12 00:18:47 +0530

    Revert "msm8998-common: Update DPM/IMS from jasmine - V11.0.15.0.QDIMIXM"
    This reverts commit fb6d3bb8c57f211d840cbc28f6ddbc229f05d3d5.

--- extract-files.sh
+++ extract-files.sh
@@ -63,8 +63,15 @@ function blob_fixup() {
         lib64/libwfdnative.so | lib/libwfdnative.so )
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
+        system_ext/etc/init/dpmd.rc)
+            sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
+            ;;
+        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml | \
         system_ext/etc/permissions/qcrilhook.xml | system_ext/etc/permissions/telephonyservice.xml )
             sed -i "s/\/product\/framework\//\/system_ext\/framework\//g" "${2}"
+            ;;
+        system_ext/lib64/libdpmframework.so )
+            sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
             ;;
         vendor/etc/permissions/com.fingerprints.extension.xml )
             sed -i 's/\/system\/framework\//\/vendor\/framework\//g' "${2}"
