880a8f8f83ddf7b443635adcaaa7546bd3218c37
SirRGB <sirrgb@proton.me>
2023-01-15 22:43:54 +0100

    msm8998-common: update defconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
-TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
+TARGET_KERNEL_CONFIG := oneplus5_defconfig
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
