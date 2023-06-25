c8b87a2d4bc69f42afa10ab926e97276901233f9
lobo-1978 <wolf45734@gmail.com>
2023-06-25 18:00:20 +1000

    msm8998-common: DeviceSettings: Remove touchscreen gestures.
    Change-Id: Ic6a29f22c051d747a94dc74a5cde6044004d794d

--- DeviceSettings/src/com/aurora/device/DeviceSettings/Utils.java
+++ DeviceSettings/src/com/aurora/device/DeviceSettings/Utils.java
@@ -17,9 +17,6 @@
 */
 package com.aurora.device.DeviceSettings;
 
-import android.content.res.Resources;
-import android.util.Log;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -28,8 +25,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Utils {
-
-    private static final String TAG = Utils.class.getSimpleName();
 
     /**
      * Write a string value to the specified file.
@@ -105,25 +100,5 @@ public class Utils {
             return fileValue;
         }
         return defValue;
-    }
-
-    public static String getLocalizedString(final Resources res,
-                                            final String stringName,
-                                            final String stringFormat) {
-        final String name = stringName.toLowerCase().replace(" ", "_");
-        final String nameRes = String.format(stringFormat, name);
-        return getStringForResourceName(res, nameRes, stringName);
-    }
-
-    public static String getStringForResourceName(final Resources res,
-                                                  final String resourceName,
-                                                  final String defaultValue) {
-        final int resId = res.getIdentifier(resourceName, "string", "com.aurora.device.DeviceSettings");
-        if (resId <= 0) {
-            Log.e(TAG, "No resource found for " + resourceName);
-            return defaultValue;
-        } else {
-            return res.getString(resId);
-        }
     }
 }
