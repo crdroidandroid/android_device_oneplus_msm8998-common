86b01afeea257683dd64d6669ba9af7840d293e8
Hemant Sharma <hemantbeast@gmail.com>
2023-09-12 21:38:17 +0530

    msm8998-common: DeviceSettings: Add audio gain controls
    Change-Id: Ia214d1cfb5a611f5308f04c3c8b47ee38717e334
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

---
+++ DeviceSettings/src/com/lineageos/device/DeviceSettings/audio/SpeakerGainPreference.java
@@ -0,0 +1,103 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+* Copyright (C) 2020 The Android Ice Cold Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.lineageos.device.DeviceSettings.audio;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.ContentObserver;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Button;
+import android.os.Bundle;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceViewHolder;
+
+import com.lineageos.device.DeviceSettings.DeviceSettings;
+import com.lineageos.device.DeviceSettings.R;
+import com.lineageos.device.DeviceSettings.Utils;
+
+public class SpeakerGainPreference extends Preference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private static final String FILE_LEVEL = "/sys/kernel/sound_control/speaker_gain";
+
+    private int mMinValue;
+    private int mMaxValue;
+    private SeekBar mSeekBar;
+    private int mOldStrength;
+
+    public SpeakerGainPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mMinValue = -10;
+        mMaxValue = 20;
+        
+        setLayoutResource(R.layout.preference_seek_bar);
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(FILE_LEVEL);
+    }
+
+    public static String getValue(Context context) {
+        return Utils.getFileValue(FILE_LEVEL, "0");
+    }
+
+    private void setValue(String newValue) {
+        Utils.writeValue(FILE_LEVEL, newValue);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(DeviceSettings.KEY_AUDIO_SPEAKER, newValue);
+        editor.commit();
+    }
+
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_AUDIO_SPEAKER, "0");
+        Utils.writeValue(FILE_LEVEL, storedValue);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        mOldStrength = Integer.parseInt(getValue(getContext()));
+        mSeekBar = (SeekBar) holder.findViewById(R.id.seekbar);
+        mSeekBar.setMax(mMaxValue - mMinValue);
+        mSeekBar.setProgress(mOldStrength - mMinValue);
+        mSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        // NA
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // NA
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        setValue(String.valueOf(seekBar.getProgress() + mMinValue));
+    }
+}
