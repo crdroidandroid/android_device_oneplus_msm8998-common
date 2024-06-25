0fe91346944f16b8928404e2d70a27c754d2bd9a
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    msm8998-common: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,6 +46,9 @@ TARGET_KERNEL_CONFIG := oneplus5_defconfig
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
+# A/B
+AB_OTA_UPDATER := false
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
