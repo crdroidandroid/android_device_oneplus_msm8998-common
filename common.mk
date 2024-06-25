7cb7ca12f118c16085c9e909023b8613e9a0d7f4
Georg Veichtlbauer <georg@vware.at>
2024-06-25 23:02:34 +0200

    msm8998-common: Build vendor variant of libpower
    Change-Id: Id7fe8a2598ffb93be4635424c86100a88aea8780

--- common.mk
+++ common.mk
@@ -391,6 +391,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
+    libpower.vendor \
     libsensorndkbridge
 
 # Shims
