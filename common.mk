b29f1b4c184e817146511b3a7ac0e8fc90ec2409
Zed <31599865+Zed9K@users.noreply.github.com>
2024-07-08 04:06:32 +0300

    msm8998-common: Signed keys

--- common.mk
+++ common.mk
@@ -467,3 +467,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Sign builds
+-include vendor/lineage-priv/keys/keys.mk
