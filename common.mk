35b4d45fb98c9b733eb620238a971415e8b1bf93
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    msm8998-common: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- common.mk
+++ common.mk
@@ -149,14 +149,12 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
     libdisplayconfig \
     libqdMetaData.system \
     libtinyxml \
-    memtrack.msm8998
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/FOSSConfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/FOSSConfig.xml
